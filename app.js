ba769ecccfed9d16932bc2e0dbe842a69401369e
tophat8855 <hrfarley@gmail.com>
2015-03-09 22:59:06 -0700

    animated highlighting cycles through elements

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
+  var homeArray = ["mansion", "apartment", "shack", "house"];
+  var spouseArray = ["spouse1", "spouse2", "spouse3", "spouse4"];
+  var kidsArray = ["kids1", "kids2", "kids3", "kids4"];
+  var vehicleArray = ["vehicle1", "vehicle2", "vehicle3", "vehicle4"];
+  var everythingArray = homeArray.concat(spouseArray, kidsArray, vehicleArray);
+
   function initialize() {
-    var homeArray = ["mash", "apartment", "shack", "house"];
-    var spouseArray = ["spouse1", "spouse2", "spouse3", "spouse4"];
-    var kidsArray = ["kids1", "kids2", "kids3", "kids4"];
-    var vehicleArray = ["vehicle1", "vehicle2", "vehicle3", "vehicle4"];
-    var everythingArray = homeArray.concat(spouseArray, kidsArray, vehicleArray);
 
     var number = 5; //this is just for practice
 
@@ -17,45 +18,51 @@ $(document).ready(function() {
     mash(resultArray, number);
   }
 
-  function cycleThrough() {
-    var homeArray = ["mash", "apartment", "shack", "house"];
-    var spouseArray = ["spouse1", "spouse2", "spouse3", "spouse4"];
-    var kidsArray = ["kids1", "kids2", "kids3", "kids4"];
-    var vehicleArray = ["vehicle1", "vehicle2", "vehicle3", "vehicle4"];
-    var everythingArray = homeArray.concat(spouseArray, kidsArray, vehicleArray);
-    
+  function currentIdFinder(){
+    return $('.highlighted')[0].id;
+  }
 
+  function moveToNextItemInArray() {
+    var currentId = currentIdFinder();
+    var nextId;
+    if(everythingArray.indexOf(currentId) + 1 < everythingArray.length) {
+      nextId = everythingArray[everythingArray.indexOf(currentId) + 1];
+    } else {
+      nextId = everythingArray[0];
+    }
+    $('#' + currentId).toggleClass('highlighted');
+    $('#' + nextId).toggleClass('highlighted');
   }
 
   function mash(resultArray, number) {
     $('body').on('click', '#mash', function(event) {
-      while(resultArray.length < number) {
-
-      }
-
-      var randHome = Math.random();
-    if (randHome < 0.25) {
-      newHome = 'mansion';
-    } else if (randHome < 0.5) {
-      newHome = 'apartment';
-    } else if (randHome < 0.75) {
-      newHome = 'shack';
-    } else if (randHome < 1) {
-      newHome = 'house';
+      //every 7/10 of a second highlight the next item in the everything Array and unlight the one that was highlighted
+      window.setInterval(moveToNextItemInArray, 667);
     }
 
-    var randSpouse = Math.ceil(Math.random() * 4);
-    newSpouse = $('#spouse' + randSpouse).val();
-
-    var randKid = Math.ceil(Math.random() * 4);
-    newKids = $('#kids' + randKid).val();
-
-    var randVehicle = Math.ceil(Math.random() * 4);
-    newVehicle = $('#vehicle' + randVehicle).val();
-
-    var results = '<p> You will marry ' + newSpouse + ' and live in a fabulous ' + newHome + '! Have fun cruising the town in your ' + newVehicle + ' with ' + newKids + ' kids!</p>';
-    $('body').append(results);
-    });
+    //   var randHome = Math.random();
+    // if (randHome < 0.25) {
+    //   newHome = 'mansion';
+    // } else if (randHome < 0.5) {
+    //   newHome = 'apartment';
+    // } else if (randHome < 0.75) {
+    //   newHome = 'shack';
+    // } else if (randHome < 1) {
+    //   newHome = 'house';
+    // }
+    //
+    // var randSpouse = Math.ceil(Math.random() * 4);
+    // newSpouse = $('#spouse' + randSpouse).val();
+    //
+    // var randKid = Math.ceil(Math.random() * 4);
+    // newKids = $('#kids' + randKid).val();
+    //
+    // var randVehicle = Math.ceil(Math.random() * 4);
+    // newVehicle = $('#vehicle' + randVehicle).val();
+    //
+    // var results = '<p> You will marry ' + newSpouse + ' and live in a fabulous ' + newHome + '! Have fun cruising the town in your ' + newVehicle + ' with ' + newKids + ' kids!</p>';
+    // $('body').append(results);
+   );
   }
 
   // $('.ui.form')
