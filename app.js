998dd376e45b77cbfbf13360c55087d5d764e46c
tophat8855 <hrfarley@gmail.com>
2015-04-20 22:49:29 -0700

    cycling and disabling logic mostly works.

--- app.js
+++ app.js
@@ -4,40 +4,92 @@ $(document).ready(function() {
   var kidsArray = ["kids1", "kids2", "kids3", "kids4"];
   var vehicleArray = ["vehicle1", "vehicle2", "vehicle3", "vehicle4"];
   var everythingArray = homeArray.concat(spouseArray, kidsArray, vehicleArray);
+  var number = 5; //this is just for practice
+  var count = 1; //debugging purposes
+  var currentIndex = 0;
+  var timer;
+
+  var results = {};
 
   function initialize() {
-
-    var number = 5; //this is just for practice
-
-    var resultArray = [];
-
-    var resultSpouse;
-    var resultKids;
-    var resultVehicle;
-    var resultHome;
-    mash(resultArray, number);
+    mash();
   }
 
-  function currentIdFinder(){
-    return $('.highlighted')[0].id;
+  function checkIfDone() {
+    if (results.home && results.spouse && results.kids && results.vehicle) {
+      window.clearInterval(timer);
+      console.log("ended the game");
+    }
   }
+
+  // function currentIdFinder(){
+  //   return $('.highlighted')[0].id;
+  // }
 
   function moveToNextItemInArray() {
-    var currentId = currentIdFinder();
-    var nextId;
-    if(everythingArray.indexOf(currentId) + 1 < everythingArray.length) {
-      nextId = everythingArray[everythingArray.indexOf(currentId) + 1];
+    console.log("here");
+    currentId = everythingArray[currentIndex];
+    if (currentIndex + 1 < everythingArray.length) {
+      nextId = everythingArray[currentIndex + 1];
+      nextIdIndex = currentIndex + 1;
     } else {
       nextId = everythingArray[0];
+      nextIdIndex = 0;
     }
+
     $('#' + currentId).toggleClass('highlighted');
     $('#' + nextId).toggleClass('highlighted');
+
+    if (!checkForDisabling(nextIdIndex, nextId)) {
+      currentIndex = nextIdIndex;
+    } else {
+      $('#' + nextId).toggleClass('highlighted');
+    }
+
+
+    // // count += 1;
+    // // var currentId = currentIdFinder();
+    // var nextId;
+    // var nextIdIndex;
+    // var currentId = everythingArray[currentIndex];
+    //
+    // //checking for end of everythingArray
+    //
+    // $('#' + currentId).toggleClass('highlighted');
+    // $('#' + nextId).toggleClass('highlighted');
+    // // checkForDisabling(nextIdIndex, nextId);
+    // currentIndex += 1;
+    // console.log(currentIndex);
+    //
+    //
+    // if(currentIndex + 1 < everythingArray.length) {
+    //   nextIdIndex = currentIndex + 1;
+    //   nextId = everythingArray[nextIdIndex];
+    // } else {
+    //   nextId = everythingArray[0];
+    // }
+    count += 1;
   }
 
-  function mash(resultArray, number) {
+  function checkForDisabling(nextIdIndex, nextId) {
+    if(count % number === 0) {
+
+      console.log("yep!");
+      $('#' + nextId).prop('disabled',true);
+      everythingArray.splice(nextIdIndex, 1);
+      console.log(everythingArray);
+
+      return true;
+      // currentIndex -= 1;
+    } else {
+      return false;
+    }
+  }
+
+  function mash() {
     $('body').on('click', '#mash', function(event) {
       //every 7/10 of a second highlight the next item in the everything Array and unlight the one that was highlighted
-      window.setInterval(moveToNextItemInArray, 667);
+      timer = window.setInterval(moveToNextItemInArray, 667);
     }
 
     //   var randHome = Math.random();
